3a18d8b4b1a230ed713d77e6d499f70adca4eec0
Guillermo Guirao Aguilar <contact@guillermoguiraoaguilar.com>
2019-08-11 17:39:54 +0200

    Improved ops files and added CAA record

--- ops/cloudfront.tf
+++ ops/cloudfront.tf
@@ -1,15 +1,17 @@
 resource "aws_cloudfront_distribution" "s3_distribution" {
-  enabled = true
-  aliases = ["www.bitelio.com", "bitelio.com"]
+  enabled             = true
+  aliases             = ["bitelio.com", "www.bitelio.com"]
   default_root_object = "index.html"
-  is_ipv6_enabled = true
+  is_ipv6_enabled     = true
+
   origin {
     domain_name = aws_s3_bucket.bitelio-com.bucket_regional_domain_name
-    origin_id = "S3-bitelio"
+    origin_id   = "S3-bitelio"
   }
+
   default_cache_behavior {
-    allowed_methods = ["GET", "HEAD"]
-    cached_methods = ["GET", "HEAD"]
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
     target_origin_id = "S3-bitelio"
 
     forwarded_values {
@@ -21,24 +23,27 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     }
 
     viewer_protocol_policy = "redirect-to-https"
-    min_ttl = 0
-    default_ttl = 3600
-    max_ttl = 86400
-    compress = true
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
   }
+
   viewer_certificate {
-    acm_certificate_arn = "arn:aws:acm:us-east-1:561291244739:certificate/b2a1a1fe-6189-415b-b34c-29fbafed3551"
-    ssl_support_method = "sni-only"
+    acm_certificate_arn      = "arn:aws:acm:us-east-1:561291244739:certificate/b2a1a1fe-6189-415b-b34c-29fbafed3551"
+    ssl_support_method       = "sni-only"
     minimum_protocol_version = "TLSv1.1_2016"
   }
+
   restrictions {
     geo_restriction {
       restriction_type = "none"
     }
   }
+
   custom_error_response {
-    error_code = 404
-    response_code = 404
+    error_code         = 404
+    response_code      = 404
     response_page_path = "/404.html"
   }
 }
--- ops/route53.tf
+++ ops/route53.tf
@@ -1,60 +1,54 @@
-resource "aws_route53_zone" "bitelio-com-public" {
+resource "aws_route53_zone" "primary" {
   name = "bitelio.com"
-  comment = ""
 }
 
 resource "aws_route53_record" "bitelio-com-A" {
-  zone_id = "Z55UFJUSNXP2N"
-  name = "bitelio.com"
-  type = "A"
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "bitelio.com"
+  type    = "A"
 
   alias {
-    name = "d1ne4wzzp1ik1q.cloudfront.net"
-    zone_id = "Z2FDTNDATAQYW2"
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
     evaluate_target_health = false
   }
-}
-
-resource "aws_route53_record" "bitelio-com-NS" {
-  zone_id = "Z55UFJUSNXP2N"
-  name = "bitelio.com"
-  type = "NS"
-  records = ["ns-1778.awsdns-30.co.uk.", "ns-934.awsdns-52.net.", "ns-1478.awsdns-56.org.", "ns-429.awsdns-53.com."]
-  ttl = "172800"
-}
-
-resource "aws_route53_record" "bitelio-com-SOA" {
-  zone_id = "Z55UFJUSNXP2N"
-  name = "bitelio.com"
-  type = "SOA"
-  records = ["ns-1478.awsdns-56.org. awsdns-hostmaster.amazon.com. 1 7200 900 1209600 86400"]
-  ttl = "900"
-}
-
-resource "aws_route53_record" "_40cdaec37a94cd19808135404a224015-bitelio-com-CNAME" {
-  zone_id = "Z55UFJUSNXP2N"
-  name = "_40cdaec37a94cd19808135404a224015.bitelio.com"
-  type = "CNAME"
-  records = ["_af76a6f80848ff7abfcc1aa11990dc9c.hkvuiqjoua.acm-validations.aws."]
-  ttl = "300"
 }
 
 resource "aws_route53_record" "www-bitelio-com-A" {
-  zone_id = "Z55UFJUSNXP2N"
-  name = "www.bitelio.com"
-  type = "A"
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "www.bitelio.com"
+  type    = "A"
 
   alias {
-    name = "d1ne4wzzp1ik1q.cloudfront.net"
-    zone_id = "Z2FDTNDATAQYW2"
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
     evaluate_target_health = false
   }
 }
 
+resource "aws_route53_record" "caa" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "bitelio.com."
+  type    = "CAA"
+  ttl     = "3600"
+  records = [
+    "0 issue \"amazon.com\"",
+    "0 issuewild \"amazon.com\"",
+  ]
+}
+
+resource "aws_route53_record" "_40cdaec37a94cd19808135404a224015-bitelio-com-CNAME" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "_40cdaec37a94cd19808135404a224015.bitelio.com"
+  type    = "CNAME"
+  records = ["_af76a6f80848ff7abfcc1aa11990dc9c.hkvuiqjoua.acm-validations.aws."]
+  ttl     = "300"
+}
+
 resource "aws_route53_record" "_316cf624d6e0ae3892468c01c5afff41-www-bitelio-com-CNAME" {
-  zone_id = "Z55UFJUSNXP2N"
-  name = "_316cf624d6e0ae3892468c01c5afff41.www.bitelio.com"
-  type = "CNAME"
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = "_316cf624d6e0ae3892468c01c5afff41.www.bitelio.com"
+  type    = "CNAME"
   records = ["_42ae1ec0157ae4c7d45676813e31ab66.hkvuiqjoua.acm-validations.aws."]
-  ttl = "300"
+  ttl     = "300"
 }
