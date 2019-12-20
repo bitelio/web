26615f25b4aa8a85e0293d726e2f2000c65cfe14
Guillermo Guirao Aguilar <contact@guillermoguiraoaguilar.com>
2019-12-20 23:49:17 +0100

    Simplified web

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,3 @@
 require('./particles');
 
 window.dataLayer = window.dataLayer || [];
-window.contact = function() {
-  window.dataLayer.push(('event', 'email'))
-  window.location.href = 'mailto:info@bitelio.com'
-}
